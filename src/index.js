5c291761871d4c8ec195b1fb41a609ec8197b1f5
velteren <senior.saveliev2012@yandex.ru>
2021-12-01 21:08:12 +0300

    add final solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let words = expr.split('**********');
+    let result = [];
+    for (let i = 0; i< words.length; i++) {
+    	result.push(decodeOneWord(words[i]));
+    }
+    
+    function decodeOneWord(word) {
+    	let fullWord = '';
+    	let letters = word.match(/.{1,10}/g);
+      for (let i = 0; i < letters.length; i++) {
+      	fullWord += decodeOneLetter(letters[i]);
+      }
+      return fullWord;
+    }
+    
+    function decodeOneLetter(letter) {
+    	let arr = letter.match(/.{1,2}/g);
+      let res = '';
+      for (let i = 0; i < arr.length; i++) {
+      	if (arr[i] != '00') {
+        	if (arr[i] == '10') {
+          	res += '.';
+          } else res += '-';
+        }
+      }
+      let finalLetter = MORSE_TABLE[res];
+      return finalLetter;
+    }
+    
+    return result.join(' ');
 }
 
 module.exports = {
-    decode
+    decode,
 }
